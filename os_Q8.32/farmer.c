5f2b5d09639428ffa91eda49d1a3e249ffe30f3c
Joy <joylin000905@gmail.com>
2024-04-29 23:17:21 +0800

    Q8.32,過橋順序交換

---
+++ os_Q8.32/farmer.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define MAX_FARMERS 20
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t northbound = PTHREAD_COND_INITIALIZER;
+pthread_cond_t southbound = PTHREAD_COND_INITIALIZER;
+
+int on_bridge = 0;
+
+void *Northbound_farmer(void *arg)
+{
+    pthread_mutex_lock(&mutex);
+
+    while (on_bridge)
+    {
+        printf("Northbound farmer is waiting.\n");
+        pthread_cond_wait(&northbound, &mutex);
+    }
+
+    on_bridge = 1;
+    printf("Northbound farmer is crossing the bridge.\n");
+    pthread_mutex_unlock(&mutex);
+
+    sleep(rand() % 3 + 1);
+
+    pthread_mutex_lock(&mutex);
+    on_bridge = 0;
+    pthread_cond_signal(&southbound);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void *Southbound_farmer(void *arg)
+{
+    pthread_mutex_lock(&mutex);
+
+    while (on_bridge)
+    {
+        printf("Southbound farmer is waiting.\n");
+        pthread_cond_wait(&southbound, &mutex);
+    }
+
+    on_bridge = 1;
+    printf("Southbound farmer is crossing the bridge.\n");
+    pthread_mutex_unlock(&mutex);
+
+    sleep(rand() % 3 + 1);
+
+    pthread_mutex_lock(&mutex);
+    on_bridge = 0;
+    pthread_cond_signal(&northbound);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t north_thread[MAX_FARMERS], south_thread[MAX_FARMERS];
+
+    for (int i = 0; i < MAX_FARMERS; i++)
+    {
+        pthread_create(&north_thread[i], NULL, Northbound_farmer, NULL);
+        pthread_create(&south_thread[i], NULL, Southbound_farmer, NULL);
+    }
+
+    for (int i = 0; i < MAX_FARMERS; i++)
+    {
+        pthread_join(north_thread[i], NULL);
+        pthread_join(south_thread[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
