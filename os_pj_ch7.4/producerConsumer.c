53fbe3dc1b5f8d522263860e1809a9a6b9349e38
Joy <joylin000905@gmail.com>
2024-05-08 22:20:36 +0800

    pj 7.4 fix no wait

--- os_pj_ch7.4/producerConsumer.c
+++ os_pj_ch7.4/producerConsumer.c
@@ -13,13 +13,14 @@ sem_t full;
 
 int insertPointer = 0, removePointer = 0;
 
-void InsertItem(buffer_item item);
-void RemoveItem();
-void *Producer(void *param);
-void *Consumer(void *param);
+void InsertItem(buffer_item item, int pid);
+void RemoveItem(int cid);
+void *Producer(void *pid);
+void *Consumer(void *cid);
 
 int main(int argc, char *argv[])
 {
+    pthread_t producers[5], consumers[5];
     int sleepTime, producerNum, consumerNum;
 
     if (argc != 4)
@@ -32,28 +33,47 @@ int main(int argc, char *argv[])
     producerNum = atoi(argv[2]);
     consumerNum = atoi(argv[3]);
 
+    if (producerNum > 5 || consumerNum > 5)
+    {
+        printf("producer and consumer max 5\n");
+    }
+
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, 5);
     sem_init(&full, 0, 0);
     srand(time(0));
 
+    int id[5] = {1, 2, 3, 4, 5};
+
     for (int i = 0; i < producerNum; i++)
     {
-        pthread_t thread;
-        pthread_create(&thread, NULL, Producer, NULL);
+        pthread_create(&producers[i], NULL, Producer, (void *)&id[i]);
     }
 
     for (int i = 0; i < consumerNum; i++)
     {
-        pthread_t thread;
-        pthread_create(&thread, NULL, Consumer, NULL);
+        pthread_create(&consumers[i], NULL, Consumer, (void *)&id[i]);
+    }
+
+    for (int i = 0; i < producerNum; i++)
+    {
+        pthread_join(producers[i], NULL);
+    }
+
+    for (int i = 0; i < consumerNum; i++)
+    {
+        pthread_join(consumers[i], NULL);
     }
 
     sleep(sleepTime);
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
     return 0;
 }
 
-void *Producer(void *param)
+void *Producer(void *pid)
 {
     buffer_item item;
     int r;
@@ -62,42 +82,44 @@ void *Producer(void *param)
     sleep(r);
     item = rand();
 
-    InsertItem(item);
+    InsertItem(item, *((int *)pid));
 }
 
-void *Consumer(void *param)
+void *Consumer(void *cid)
 {
     int r;
 
     r = rand() % 5;
     sleep(r);
 
-    RemoveItem();
+    RemoveItem(*((int *)cid));
 }
 
-void InsertItem(buffer_item item)
+void InsertItem(buffer_item item, int pid)
 {
     sem_wait(&empty);
-
     pthread_mutex_lock(&mutex);
 
-    buffer[insertPointer++] = item;
-    insertPointer %= 5;
-    printf("Producer produced %d \n", item);
+    buffer[insertPointer] = item;
+
+    printf("Producer #%d inserted item %d at buffer position %d\n", pid, item, insertPointer);
+
+    insertPointer = (insertPointer + 1) % 5;
 
     pthread_mutex_unlock(&mutex);
     sem_post(&full);
 }
 
-void RemoveItem()
+void RemoveItem(int cid)
 {
     sem_wait(&full);
-
     pthread_mutex_lock(&mutex);
 
-    printf("Consumer consumed %d \n", buffer[removePointer]);
-    buffer[removePointer++] = -1;
-    removePointer %= 5;
+    buffer_item item = buffer[removePointer];
+
+    printf("Consumer #%d removed item %d at buffer position %d\n", cid, item, removePointer);
+
+    removePointer = (removePointer + 1) % 5;
 
     pthread_mutex_unlock(&mutex);
     sem_post(&empty);
